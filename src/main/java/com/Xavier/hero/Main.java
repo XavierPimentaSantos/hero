2d9b2ae843ff0d5c83eee7342ce834088af6a077
Xavier <up202108894@up.pt>
2022-11-12 12:08:02 +0000

    passo 5 corrigido

--- src/main/java/com/Xavier/hero/Main.java
+++ src/main/java/com/Xavier/hero/Main.java
@@ -3,20 +3,8 @@ package com.Xavier.hero;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            Terminal terminal = new;
-            DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Hello World!");
     }
 }
