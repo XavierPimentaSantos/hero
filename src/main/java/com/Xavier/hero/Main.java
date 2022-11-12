a9a88183bd6704e2cf9037281c00d52315c0a4c8
Xavier <up202108894@up.pt>
2022-11-12 11:43:05 +0000

    passo 5

--- src/main/java/com/Xavier/hero/Main.java
+++ src/main/java/com/Xavier/hero/Main.java
@@ -1,7 +1,22 @@
 package com.Xavier.hero;
 
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Terminal terminal = new;
+            DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
